a5f948633ec04dda45dfd5f92cd96a8ae4c979e0
alenkdaniel <alenkdaniel77342@gmail.com>
2023-09-12 16:12:36 +0530

    javascriptnew

--- script.js
+++ script.js
@@ -1,1 +1,92 @@
 console.log("hi hello world")
+var a=1;
+console.log("a",a);
+console.log("a'c'b");
+console.log('a"c"b');
+console.log(`${a}`);
+var a;
+a=5;
+console.log("a",a);
+a=6;
+console.log("a",a);
+var a=1;
+console.log("a",a);
+// let b;
+// console.log("a",a);
+// let b=7;
+// console.log("b",b;
+// const c;
+// console.log("c",c);
+// const c=1;
+// console.log("c",c);
+// const c=12;
+// console.log("c",c);
+// var a,d;
+// console.log("a",a,d);
+// a=6;
+// d=9;
+// console.log("a",a,d);
+let v,u;
+console.log("a",v,u);
+v=1;
+u=2;
+console.log("a",v,u);
+window.console.log("hai");
+console.log("z",z);var a1=10;
+// let b1=11;
+// const c1=12;
+// console.log(a1);
+// console.log(b1);
+// console.log(c1);
+
+var z;
+z=8;
+
+console.log(a1);
+// var a1=10;
+// let b1=11;
+// const c1=12;
+// console.log(a1);
+// console.log(b1);
+// console.log(c1);var a1=10;
+// let b1=11;
+// const c1=12;
+// console.log(a1);
+// console.log(b1);
+// console.log(c1);
+
+ var a2=10;
+console.log(a2);
+let a3=10;
+console.log(a2);
+let a4="alen";
+console.log(a4);
+let a5="alenk";
+console.log(a5);
+let a6=true;
+console.log(a6);
+let a7=null;
+console.log(a7);
+let a8=undefined;
+console.log(a8);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
