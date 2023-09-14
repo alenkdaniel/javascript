8a20f0e7be0470108e6e3b84e8cb18daaa90059f
alenkdaniel <alenkdaniel77342@gmail.com>
2023-09-14 16:59:35 +0530

    arrays, objects, conditional statements

--- script.js
+++ script.js
@@ -71,6 +71,86 @@ let a8=undefined;
 console.log(a8);
 
 
+//14-09-2023
+console.log("hi hello world");
+let arr = ["apple", "5", "true"];
+console.log("array elements is:", arr);
+let arr2 = new Array("era");
+console.log("array:", arr2);
+let arr3 = ["1", "2", "false"];
+console.log("array length :", arr.length);
+let arr6 = ["false", "1"];
+let arr7 = ["true", "2"];
+console.log("final array :", arr.join());
+console.log(arr[0]);
+let employee = {
+  name: "adarsh",
+  age: 26,
+};
+console.log("employee", employee);
+let student = {
+  name: "amal",
+  age: 20,
+};
+let arremployee = [{ name: "alen" }, { age: 22 }];
+console.log(arremployee[0].name);
+let employee1 = {
+  name: "alen",
+  age: 22,
+};
+let result = JSON.stringify(employee1);
+console.log("result", result);
+console.log("type", typeof result);
+console.log("type", typeof employee1);
+let emp = JSON.parse(result);
+console.log("parse", emp);
+// let az1=prompt("enter a  first value");
+// ;
+// // let az2=prompt("enter a  second value");
+// ;
+// if(az1>az2)
+// {
+//     console.log("greater is :",az1);
+// }
+// else if(az1==az2){
+//     console.log("the numbers are equal");
+// }
+// else {
+//     console.log("greater is :",az2);
+// }
+// let az="alen";
+// switch(az){
+//     case 1:console.log(the)
+// }
+
+// let a = prompt("Enter a number : ");
+
+// switch (Number(a)) {
+//   case 1:
+//     console.log("Number is one");
+//     break;
+
+//   case 2:
+//     console.log("Number is 2");
+//     break;
+
+//   default:
+//     console.log("Default block has been executed");
+// }
+
+let b = prompt("enter a day");
+switch (b) {
+  case "monday":
+    console.log("monday");
+    break;
+  case "tuesday":
+    console.log("tuesday");
+  default:
+    console.log("default day");
+}
+
+
+
 
 
 
