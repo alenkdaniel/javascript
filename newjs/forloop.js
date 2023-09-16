94d713f2448ad7b504d685200c8369b1e0e675bf
alenkdaniel <alenkdaniel77342@gmail.com>
2023-09-16 16:06:14 +0530

    loop,functions

---
+++ newjs/forloop.js
@@ -0,0 +1,38 @@
+// for(let x=0; x<=10;x++){
+//     console.log(x);
+// }
+let x1=30;
+let y1=20;
+// while(x1<=y1){
+//     console.log(x1);
+//    x1++;
+// }
+// do{
+//     console.log(x1);
+//     x1++;
+// }while(x1<=y1)
+// let ar=[10,20,"alen",false,null];
+// for(i in ar){
+//     if(i>=3){
+//         break;
+//     }
+//     console.log(ar[i]);
+// }
+// let ar1=[10,"alen",null,30,true];
+// ar1.forEach(function(value,index){ // call back function 
+//     console.log(value);
+//     console.log(index);
+// }) 
+for(i=0;i<=15;i++){
+    if(i==10){
+        continue;
+    }
+    console.log(i);
+}
+// arrow function
+var z1= ()=>{  
+    console.log("hello");
+};
+z1();
+
+    
